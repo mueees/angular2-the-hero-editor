73a428abbe70a52f04684554678025eb1a497e36
vitalii.mishchenko <vitalii.mishchenko@globallogic.com>
2016-06-17 20:43:13 +0300

    'Stage2'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,12 @@
 import { Component } from '@angular/core';
-
-const HEROES:Hero[] = [
-    {id: 11, name: 'Mr. Nice'},
-    {id: 12, name: 'Narco'},
-    {id: 13, name: 'Bombasto'},
-    {id: 14, name: 'Celeritas'},
-    {id: 15, name: 'Magneta'},
-    {id: 16, name: 'RubberMan'},
-    {id: 17, name: 'Dynama'},
-    {id: 18, name: 'Dr IQ'},
-    {id: 19, name: 'Magma'},
-    {id: 20, name: 'Tornado'}
-];
-
-export class Hero {
-    id:number;
-    name:string;
-}
+import { HeroDetailComponent } from './hero-detail.component';
+import {Hero} from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
+    directives: [HeroDetailComponent], 
+    providers: [HeroService],
     styles: [`
       .selected {
         background-color: #CFD8DC !important;
@@ -80,28 +67,22 @@ export class Hero {
   </li>
 </ul>
 
-<div *ngIf="selectedHero">
-  <h2>{{selectedHero.name}} details!</h2>
-  <div><label>id: </label>{{selectedHero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-  </div>
-</div>
+<my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `
 })
 
 export class AppComponent {
+	constructor(private heroService: HeroService) { }
+
+	ngOnInit() {
+		this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+ 	}
+
     title = 'Tour of Heroes';
 
     selectedHero:Hero;
 
-    hero:Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
-
-    heroes = HEROES;
+    heroes: Hero[];
 
     onSelect(hero:Hero) {
         this.selectedHero = hero;
