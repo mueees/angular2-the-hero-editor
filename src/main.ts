717227401b914364c3c665550d5a5a8da7a785a3
mue <mue.miv@gmail.com>
2016-07-05 23:03:54 +0300

    add routing

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { APP_ROUTER_PROVIDERS } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 if (process.env.ENV === 'production') {
     enableProdMode();
 }
 
-bootstrap(AppComponent, []);
+bootstrap(AppComponent, [
+    APP_ROUTER_PROVIDERS
+]);
